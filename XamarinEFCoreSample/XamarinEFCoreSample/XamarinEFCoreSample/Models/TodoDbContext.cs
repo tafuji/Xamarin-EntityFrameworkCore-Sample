e06ebe991a454e69214314923ae26d1f71c36957
Takeshi Fujimoto <tafuji@users.noreply.github.com>
2018-12-11 17:36:07 +0900

    Added the DbContext class.

---
+++ XamarinEFCoreSample/XamarinEFCoreSample/XamarinEFCoreSample/Models/TodoDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Xamarin.Essentials;
+
+namespace XamarinEFCoreSample.Models
+{
+    public class TodoDbContext : DbContext
+    {
+        private string _databaseName = "Todo.db";
+
+        private string _databasePath
+        {
+            get
+            {
+                try
+                {
+                    return Path.Combine(FileSystem.AppDataDirectory, _databasePath);
+                }
+                catch
+                {
+                    return _databaseName;
+                }
+            }
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite($"Data Source={_databasePath}");
+        }
+    }
+}
