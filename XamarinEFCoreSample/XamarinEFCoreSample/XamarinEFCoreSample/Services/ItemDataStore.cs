2234690c35be660b94d6ecbf68050a6247025f57
Takeshi Fujimoto <tafuji@users.noreply.github.com>
2018-12-11 17:47:49 +0900

    Added the data store class.

---
+++ XamarinEFCoreSample/XamarinEFCoreSample/XamarinEFCoreSample/Services/ItemDataStore.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XamarinEFCoreSample.Models;
+
+namespace XamarinEFCoreSample.Services
+{
+    public class ItemDataStore : IDataStore<Item>
+    {
+        public async Task<bool> AddItemAsync(Item item)
+        {
+            int count = 0;
+            using (var context = new TodoDbContext())
+            {
+                context.Add(item);
+                count = await context.SaveChangesAsync();
+            }
+            return await Task.FromResult(count == 1);
+        }
+
+        public async Task<bool> DeleteItemAsync(string id)
+        {
+            int count = 0;
+            using (var context = new TodoDbContext())
+            {
+                var item = context.Items.Where(p => p.Id == id).FirstOrDefault();
+                context.Remove<Item>(item);
+                count = await context.SaveChangesAsync();
+            }
+            return await Task.FromResult(count == 1);
+        }
+
+        public async Task<Item> GetItemAsync(string id)
+        {
+            using (var context = new TodoDbContext())
+            {
+                return await context.FindAsync<Item>(id);
+            }
+        }
+
+        public async Task<IEnumerable<Item>> GetItemsAsync(bool forceRefresh = false)
+        {
+            using (var context = new TodoDbContext())
+            {
+                return await Task.FromResult(context.Items);
+            }
+        }
+
+        public async Task<bool> UpdateItemAsync(Item item)
+        {
+            int count = 0;
+            using (var context = new TodoDbContext())
+            {
+                context.Entry(item).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                count = await context.SaveChangesAsync();
+            }
+            return await Task.FromResult(count == 1);
+        }
+    }
+}
