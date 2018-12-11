09743afcf8c4dd21c0a895f584b79335a10673f5
Takeshi Fujimoto <tafuji@users.noreply.github.com>
2018-12-11 16:17:22 +0900

    Created the initial solution.

---
+++ XamarinEFCoreSample/XamarinEFCoreSample/XamarinEFCoreSample/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using XamarinEFCoreSample.Models;
+
+namespace XamarinEFCoreSample.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
